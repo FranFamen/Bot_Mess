430524415f41406e6c839853b7b65b1cd83ff8ef
FranFamen <franciszek.saklawski@gmail.com>
2018-03-02 11:51:53 +0100

    First commit

---
+++ helloworld.py
@@ -0,0 +1,17 @@
+from fbchat import Client
+from fbchat.models import *
+import random
+import pandas
+client = Client("FranFamen", "patrycja00")
+
+thread_id = "100000210704616"
+thread_type = ThreadType.USER
+
+komp = list(pandas.read_csv("komplementy.txt"))
+
+def send_message():
+    client.send(Message(text = random.choice(komp)), thread_id =thread_id, thread_type = thread_type)
+    client.logout()
+
+
+print(send_message())
